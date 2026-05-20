65e799a6f8584fe3a44ad512ba02564aacfe5069
smdrahil16 <smdrahil16@gmail.com>
2026-05-21 00:13:08 +0530

    Fix ECS CORS

--- car-inspection-backend/src/main/java/com/example/demo/config/WebConfig.java
+++ car-inspection-backend/src/main/java/com/example/demo/config/WebConfig.java
@@ -1,22 +1,38 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    @Bean
+    public CorsFilter corsFilter() {
 
-        registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:4200",
-                        "http://rahil-car-inspection-ui.s3-website.ap-south-1.amazonaws.com"
-                )
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(false);
+        CorsConfiguration config =
+                new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "http://rahil-car-inspection-ui.s3-website.ap-south-1.amazonaws.com"
+        ));
+
+        config.setAllowedMethods(List.of(
+                "GET", "POST", "PUT", "DELETE", "OPTIONS"
+        ));
+
+        config.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
     }
 }
