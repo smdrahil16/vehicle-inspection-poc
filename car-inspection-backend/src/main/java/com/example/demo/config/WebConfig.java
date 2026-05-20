d9e573535cc7903f4ec0d169755790a38e9f5bc9
smdrahil16 <smdrahil16@gmail.com>
2026-05-21 00:51:21 +0530

    Force global CORS

--- car-inspection-backend/src/main/java/com/example/demo/config/WebConfig.java
+++ car-inspection-backend/src/main/java/com/example/demo/config/WebConfig.java
@@ -1,38 +1,18 @@
 package com.example.demo.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.List;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public CorsFilter corsFilter() {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
 
-        CorsConfiguration config =
-                new CorsConfiguration();
-
-        config.setAllowedOrigins(List.of(
-                "http://localhost:4200",
-                "http://rahil-car-inspection-ui.s3-website.ap-south-1.amazonaws.com"
-        ));
-
-        config.setAllowedMethods(List.of(
-                "GET", "POST", "PUT", "DELETE", "OPTIONS"
-        ));
-
-        config.setAllowedHeaders(List.of("*"));
-
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsFilter(source);
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
